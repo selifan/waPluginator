bb72e1a78361e49c6fb5372bd103c3db7bce30e2
Alexander Selifonov <as-works@narod.ru>
2016-09-24 19:01:08 +0300

    English language file added

---
+++ src/waPluginator.lng.en.php
@@ -0,0 +1,38 @@
+<?php
+/**
+* @package: waPluginator
+* Localization file
+* Language: English
+* @author: Alexander Selifonov < alex [at] selifan [dot] ru >
+* version 0.3
+*/
+
+return array(
+   'author_name' => 'Author Name',
+   'author_email' => 'Author Email',
+   'author_site' => 'Author web page (link)',
+   'creating_from' => 'Creating from',
+   'base_class_name' => 'Module Base Class Name',
+   'plugin_folder' => 'Module File/Folder Name',
+   'plugin_type' => 'Plugin Type',
+   'main_title' => 'Main title',
+   'description' => 'Module Description',
+   'color_scheme' => 'Color Scheme (Theme)',
+   'btn_generate' => 'Generate',
+   'btn_reverseing' => 'Code -&gt; Template',
+   'title_log' => 'Generation log',
+   'title_form' => 'Module Designer',
+
+   'size' => 'size',
+   'compile_ok' => 'Compilation successful',
+   'err_tmpdir_create' => 'Tmp sub-folder creation error. Job failed',
+   'err_no_srcarchive' => 'srcarchive parameter must be set (zip file name)',
+   'err_create_folder' => 'Create folder error',
+   'err_srcfile_not_found' => 'Source file not found',
+   'err_skipped_no_dir' => 'File skipped because of subdirectory creation fail',
+   'err_zipopen' => 'Zip file open error (not a zip?)',
+   'err_zipnotfound' => 'Zip file not found',
+   'err_compile_fail' => 'Compilation failed',
+   'err_bad_less' => 'Compile less file error',
+   'job_finished' => 'Files creation finished'
+);
